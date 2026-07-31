2f3bbf4c7e3571fc21f9452573148e08c6a2a35f
angela525 <angela617255@gmail.com>
2026-07-31 21:12:50 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dream-tree-v5-premium-redesign-1-1';
+const CACHE='dream-tree-v5-1-premium-1-1';
 const ASSETS=['./','./index.html','./manifest.json','./brand-logo.png','./favicon.ico','./favicon-16.png','./favicon-32.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
